3e34bf6f736d1e732ef3cf1fd09215e1d751cc1d
gilly <gilly@delver.com>
2012-07-06 13:49:05 +0300

    added a file

---
+++ Solution/DreamBoards.Domain/AppWalls/AppWall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MiamiApp.Domain.AppWalls
+{
+	public class AppWall
+	{
+		public Int64 Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime CreationDate { get; set; }
+		public string HeaderContentPath { get; set; }
+		public string RightContentPath { get; set; }
+		public string Url { get; set; }
+	}
+}

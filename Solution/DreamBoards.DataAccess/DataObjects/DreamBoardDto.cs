8beed8c2d3ad757d85dbcd90188dfe51c00daa36
gilly <gilly@delver.com>
2012-07-07 17:59:48 +0300

    added 'dreamboard' dto and mapping

---
+++ Solution/DreamBoards.DataAccess/DataObjects/DreamBoardDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DreamBoards.DataAccess.DataObjects
+{
+	public class DreamBoardDto
+	{
+		public long Id { get; set; }
+		public long UserId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+}
---
+++ Solution/DreamBoards.DataAccess/Mappings/DreamBoardDtoMap.cs
@@ -0,0 +1,20 @@
+﻿using DreamBoards.DataAccess.DataObjects;
+using FluentNHibernate.Mapping;
+
+namespace DreamBoards.DataAccess.Mappings
+{
+	public class DreamBoardDtoMap : ClassMap<DreamBoardDto>
+	{
+		public DreamBoardDtoMap()
+		{
+			Table("dreamboards");
+			Not.LazyLoad();
+			
+			Id(x => x.Id);
+			Map(x => x.UserId);
+			Map(x => x.Title);
+			Map(x => x.Description);
+			Map(x => x.CreatedDate);
+		}
+	}
+}

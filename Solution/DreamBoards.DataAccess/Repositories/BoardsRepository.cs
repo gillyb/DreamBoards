d1e4ae512a0ca832459bc77691e82839e5e5835b
gilly <gilly@delver.com>
2012-07-11 00:00:43 +0300

    added BoardsRepository

--- Solution/DreamBoards.DataAccess/Repositories/BoardsRepository.cs
+++ Solution/DreamBoards.DataAccess/Repositories/BoardsRepository.cs
@@ -1,15 +1,57 @@
-﻿using DreamBoards.DataAccess;
+﻿using System.Collections.Generic;
+using System.Linq;
 using DreamBoards.DataAccess.DataObjects;
 
 namespace DreamBoards.DataAccess.Repositories
 {
-	public class BoardsRepository
+	public interface IBoardsRepository
+	{
+		void CreateNewBoard(BoardDto board);
+		BoardDto LoadBoard(int boardId);
+		List<BoardDto> GetUsersBoards(long userId);
+	}
+
+	public class BoardsRepository : IBoardsRepository
 	{
 		private readonly ISessionFactoryProvider _sessionFactoryProvider;
 
+		public BoardsRepository(ISessionFactoryProvider sessionFactoryProvider)
+		{
+			_sessionFactoryProvider = sessionFactoryProvider;
+		}
+
 		public void CreateNewBoard(BoardDto board)
 		{
-			
+			using (var sessionFactory = _sessionFactoryProvider.BuildSessionFactory())
+			using (var session = sessionFactory.OpenSession())
+			{
+				session.Save(board);
+				session.Flush();
+			}
+		}
+
+		public BoardDto LoadBoard(int boardId)
+		{
+			using (var sessionFactory = _sessionFactoryProvider.BuildSessionFactory())
+			using (var session = sessionFactory.OpenSession())
+			{
+				var board = session.QueryOver<BoardDto>()
+					.Where(x => x.Id == boardId)
+					.SingleOrDefault<BoardDto>();
+				return board;
+			}
+		}
+
+		public List<BoardDto> GetUsersBoards(long userId)
+		{
+			using (var sessionFactory = _sessionFactoryProvider.BuildSessionFactory())
+			using (var session = sessionFactory.OpenSession())
+			{
+				var boards = session.QueryOver<BoardDto>()
+					.Where(x => x.UserId == userId)
+					.List<BoardDto>();
+				return boards.ToList();
+			}
 		}
 	}
 }

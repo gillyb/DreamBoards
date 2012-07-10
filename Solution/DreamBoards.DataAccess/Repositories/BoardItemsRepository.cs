7d50c4ffa8b7b9bf978e65c3caf43cd44f17c655
gilly <gilly@delver.com>
2012-07-11 00:02:48 +0300

    added BoardItemRepository

---
+++ Solution/DreamBoards.DataAccess/Repositories/BoardItemsRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DreamBoards.DataAccess.DataObjects;
+
+namespace DreamBoards.DataAccess.Repositories
+{
+	public interface IBoardItemsRepository
+	{
+		List<BoardItemDto> GetBoardItems(int boardId);
+	}
+
+	public class BoardItemsRepository : IBoardItemsRepository
+	{
+		private readonly ISessionFactoryProvider _sessionFactoryProvider;
+
+		public BoardItemsRepository(ISessionFactoryProvider sessionFactoryProvider)
+		{
+			_sessionFactoryProvider = sessionFactoryProvider;
+		}
+
+		public List<BoardItemDto> GetBoardItems(int boardId)
+		{
+			using (var sessionFactory = _sessionFactoryProvider.BuildSessionFactory())
+			using (var session = sessionFactory.OpenSession())
+			{
+				var boardItems = session.QueryOver<BoardItemDto>()
+					.Where(x => x.BoardId == boardId)
+					.List<BoardItemDto>();
+				return boardItems.ToList();
+			}
+		}
+	}
+}

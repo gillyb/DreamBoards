6a83a8f7fae19c3059d6ab002ae11ad59669641e
gilly <gilly@delver.com>
2012-07-10 14:32:31 +0300

    some modifications and additions
    Renamed DreamBoardDto to BoardDto
    Added a BoardItemDto
    Added script to create items table
    Changed around some styles

---
+++ Solution/DreamBoards.DataAccess/DataObjects/BoardItemDto.cs
@@ -0,0 +1,17 @@
+﻿namespace DreamBoards.DataAccess.DataObjects
+{
+	public class BoardItemDto
+	{
+		public int BoardId { get; set; }
+		public int ProductId { get; set; }
+		public int CatalogId { get; set; }
+
+		public string ImageUrl { get; set; }
+		public int ImagePosX { get; set; }
+		public int ImagePosY { get; set; }
+		public int ImageWidth { get; set; }
+		public int ImageHeight { get; set; }
+		public int ImageRotation { get; set; }
+		public int Layer { get; set; }
+	}
+}
--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `db3069420bf1e240a8b8e0a08300fdc045`.`dreamboards`(
+CREATE TABLE `db3069420bf1e240a8b8e0a08300fdc045`.`boards`(
 	`Id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`UserId` INT UNSIGNED NOT NULL,
 	`Title` VARCHAR(255),
@@ -6,3 +6,18 @@ CREATE TABLE `db3069420bf1e240a8b8e0a08300fdc045`.`dreamboards`(
 	`CreatedDate` DATETIME NOT NULL,
 	PRIMARY KEY (`Id`)
 ) ENGINE=INNODB; 
+
+CREATE TABLE `db3069420bf1e240a8b8e0a08300fdc045`.`board_items`(
+	`ItemId` INT NOT NULL AUTO_INCREMENT,
+	`BoardId` INT NOT NULL,
+	`ProductId` INT,
+	`CatalogId` INT,
+	`ImageUrl` VARCHAR(255) NOT NULL,
+	`PosX` INT,
+	`PosY` INT,
+	`Width` INT,
+	`Height` INT,
+	`Rotation` INT DEFAULT 0,
+	`Layer` INT DEFAULT 0,
+	PRIMARY KEY (`ItemId`)
+) ENGINE=INNODB;

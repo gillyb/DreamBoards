cf9081c25a37935498f68638d62f2e59ce5d5b0f
gilly <gilly@delver.com>
2012-07-11 17:46:38 +0300

    added settings for ImageService and a column to the db

---
+++ Solution/DreamBoards.Web/Services/ImageServiceSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace DreamBoards.Web.Services
+{
+	public static class ImageServiceSettings
+	{
+		public static string BoardImageLibrary
+		{
+			get { return ConfigurationManager.AppSettings["BoardImagesPath"]; }
+		}
+	}
+}

6c09c143b81d759a9e3183bc3cae59d834a8219f
gilly <gilly@delver.com>
2012-08-26 17:54:42 +0300

    added settings for production

--- Solution/DreamBoards.Domain/Settings/IPlatformSettings.cs
+++ Solution/DreamBoards.Domain/Settings/IPlatformSettings.cs
@@ -13,16 +13,25 @@
 
 	public class PlatformSettings : IPlatformSettings
 	{
+		// Production
+		//public string PlatformApiBaseUrl { get { return "http://platform.shopyourway.com"; } }
+		//public string PlatformSecureApiBaseUrl { get { return "http://platform.shopyourway.com"; } }
+		//public string PlatformPagesBaseUrl { get { return "//platform.shopyourway.com/app"; } }
+		//public string PlatformHomePage { get { return "http://platform.shopyourway.com"; } }
+
+		// Galileo
 		public string PlatformApiBaseUrl { get { return "http://galileoplatform.shopyourway.com"; } }
 		public string PlatformSecureApiBaseUrl { get { return "http://galileoplatform.shopyourway.com"; } }
 		public string PlatformPagesBaseUrl { get { return "//galileo.shopyourway.com/app"; } }
 		public string PlatformHomePage { get { return "http://galileo.shopyourway.com"; } }
 
+		// Shavrir
 		//public string PlatformApiBaseUrl { get { return "http://shavrirps.shopyourway.com:88"; } }
 		//public string PlatformSecureApiBaseUrl { get { return "http://shavrirps.shopyourway.com:88"; } }
 		//public string PlatformPagesBaseUrl { get { return "//ohio.local/app/"; } }
 		//public string PlatformHomePage { get { return "http://ohio.local"; } }
 
+		// Local
 		//public string PlatformApiBaseUrl { get { return "http://ohio.platform:88"; } }
 		//public string PlatformSecureApiBaseUrl { get { return "http://ohio.platform:88"; } }
 		//public string PlatformPagesBaseUrl { get { return "//ohio.local/app/"; } }
